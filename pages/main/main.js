26b52f68582d05cd7f2cee4c4c52f56d72d133f9
高明阳(平安科技智慧教育团队) <GAOMINGYANG499@pingan.com.cn>
2018-09-13 18:22:49 +0800

    2222222

---
+++ pages/main/main.js
@@ -0,0 +1,106 @@
+// pages/main/main.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:'高明阳小程序开发测试'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  clickMe:function(){
+    //this.setData({msg:"平安知鸟"});
+    wx.navigateTo({
+      url: '../list/list',
+    })
+  },
+  add:function(){
+    this.setData({
+      count:this.data.count+1
+    });
+  },
+  switch:function(e){
+    const length = this.data.objectArray.length
+    for(let i=0;i<length;++i){
+        const x = Math.floor(Math.random * length)
+        const y = Math.floor(Math.random * length)
+        const temp = this.data.objectArray[x]
+        this.data.objectArray[x] = this.data.objectArray[y]
+        this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray:this.data.objectArray
+    })
+  },
+  addToFront:function(e){
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id:length,unique:'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray:this.data.objectArray
+    })
+  },
+  addNumberToFront:function(e){
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray:this.data.numberArray
+    })
+  }
+
+
+
+})
